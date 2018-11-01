b7b1f72b1c4b812f55f1868540881853abdb9114
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2018-11-01 19:14:43 +0100

    + Végleges projekt

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world");
+		System.out.println("Juhász Gábor mondja:");
 		System.out.println("Hello world");
 
 	}
